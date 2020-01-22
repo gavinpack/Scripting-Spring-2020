31cd49fe1849774c2df9004de18d1c7e2b931c47
Gavin <gavinpack@gmail.com>
2020-01-22 11:22:54 -0700

    Review & Unity Events

---
+++ Project - 1/Assets/Scripts/Newer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class Newer : MonoBehaviour
+{
+    public float floatNumber;
+    public int scoreValue;
+    public UnityEvent uEvent;
+    
+    void Start()
+    {
+        uEvent.Invoke();
+    }
+    
+    void Update()
+    {
+        
+    }
+}
