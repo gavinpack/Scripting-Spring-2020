a44e3e32df65539338490a53fdefbf9825f4a4f2
Gavin <gavinpack@gmail.com>
2020-02-03 11:21:35 -0700

    Character Controller & Gravity

---
+++ Project - 1/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    private Vector3 _positionDirection;
+    public float speed = 5f;
+    public float gravity = -3f;
+    public float jumpForce = 10f;
+    
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        _positionDirection.x = Input.GetAxis("Horizontal")*speed;
+        if (Input.GetButtonDown("Jump"))
+        {
+            _positionDirection.y = jumpForce;
+        }
+
+        _positionDirection.y += gravity * Time.deltaTime;
+        controller.Move(_positionDirection*Time.deltaTime);
+    }
+}
