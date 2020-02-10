faa40881514c6d6fd2506c9cf9d7a2d118f746de
Gavin <gavinpack@gmail.com>
2020-02-10 11:13:25 -0700

    More Character movement, RigidBodies, and If Statements

--- Project - 1/Assets/Scripts/CharacterMovement.cs
+++ Project - 1/Assets/Scripts/CharacterMovement.cs
@@ -1,27 +1,38 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class CharacterMovement : MonoBehaviour
 {
-    public CharacterController controller;
+    private CharacterController _controller;
     private Vector3 _positionDirection;
     public float speed = 5f;
     public float gravity = -3f;
     public float jumpForce = 10f;
+    private int _jumpCount = 0;
+    public int jumpCountMax = 2;
 
     void Start()
     {
-        
+        _controller = GetComponent<CharacterController>();
     }
     
     void Update()
     {
+        if (_controller.isGrounded)
+        {
+            _positionDirection.y = 0;
+            _jumpCount = 0;
+        }
+        
         _positionDirection.x = Input.GetAxis("Horizontal")*speed;
-        if (Input.GetButtonDown("Jump"))
+        
+        if (Input.GetButtonDown("Jump") && _jumpCount < jumpCountMax)
         {
             _positionDirection.y = jumpForce;
+            _jumpCount++;
         }
 
         _positionDirection.y += gravity * Time.deltaTime;
-        controller.Move(_positionDirection*Time.deltaTime);
+        _controller.Move(_positionDirection*Time.deltaTime);
     }
 }
---
+++ Project - 1/Assets/Scripts/RigidBodyForces.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class RigidBodyForces : MonoBehaviour
+{
+    private Rigidbody _rigidBodyObj;
+    public float force = 100;
+    private Vector3 _forceVector;
+
+    private void Start()
+    {
+        _rigidBodyObj = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        _forceVector.y = force;
+        _rigidBodyObj.AddForce(_forceVector);
+    }
+}
