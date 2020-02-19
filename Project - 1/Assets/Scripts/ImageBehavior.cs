8278348ea9cdcfc373db97f866f54a1298ca0839
Gavin <gavinpack@gmail.com>
2020-02-19 11:16:19 -0700

    Image Behavior, Float Data, and Int Data

---
+++ Project - 1/Assets/Scripts/ImageBehavior.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class ImageBehavior : MonoBehaviour
+{
+    private Image imageObj;
+    void Start()
+    {
+        imageObj = GetComponent<Image>();
+    }
+    
+    void Update()
+    {
+        
+    }
+}
